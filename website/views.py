c0a6375d8bbacf7a0034a679c43d5e41d5963c3e
laur <miss.lauren.pak@gmail.com>
2023-03-04 10:51:57 +0800

    hello
    hi

---
+++ website/views.py
@@ -0,0 +1,77 @@
+from flask import Flask, Blueprint, render_template, request, url_for, redirect, session, flash
+from .models import Users, Posts, SignUps
+from datetime import timedelta, datetime
+from flask_login import login_user, logout_user, login_required, UserMixin, current_user
+from . import db
+from werkzeug.security import generate_password_hash, check_password_hash 
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField, BooleanField, ValidationError, TextAreaField
+from wtforms.validators import DataRequired, EqualTo, Length
+from wtforms.widgets import TextArea
+from flask_ckeditor import CKEditorField
+from flask_wtf.file import FileField
+import smtplib, ssl
+from email.message import EmailMessage
+
+
+views = Blueprint("views",  __name__)
+
+@views.home('/')
+def home():
+    return "hi"
+
+
+@views.route('/register',  methods=['GET', 'POST'] )
+def register():
+    if request.method == "POST":
+        name= request.form.get('name')
+        username = request.form.get('username')
+        email = request.form.get('email')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        username_exists = Users.query.filter_by(username = username).first()
+        email_exists = Users.query.filter_by(email = email).first()
+        if password1 != password2:
+            flash("Passwords don't match!")
+        elif username_exists or email_exists:
+            flash('username or email already exists!')
+        elif len(password1) < 6 or len(username) <6:
+            flash("Username or password must contain more than 6 characters")
+        else:
+            new_user = Users(name = name, username = username, email=email, password=generate_password_hash(password1))
+            db.session.add(new_user)
+            db.session.commit()
+            login_user(new_user)
+            session['loggedin'] = True
+            flash('sign up sucessful', 'info')
+            return redirect(url_for('views.home'))
+
+
+    return render_template("register.html")
+
+@views.route('/login',  methods=['GET', 'POST'] )
+def login():
+    if request.method == "POST":
+        username = request.form.get('username')
+        password = request.form.get('password')
+        permanentsesh =request.form.get('permanentsession')
+        user = Users.query.filter_by(username=username).first() #Users is database with all the users
+       #the line above will create a new child in the class User
+       #stores as first because usernames are unique and will always be the first option
+        if user:
+            if check_password_hash(user.password, password):
+                login_user(user, remember=user.email)
+                print('Logged in')
+                if permanentsesh:
+                    session.permanent = True
+                else:
+                    session.permanent = False
+                session['loggedin'] = True
+                session['email'] = user.email
+                flash('Log in sucessful', 'info ')
+                return redirect(url_for('views.home')) 
+            else:
+                print('Wrong Password')
+        else:
+            print('Username does not exist')
